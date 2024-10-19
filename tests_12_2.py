fc484357dd1c5ac4e1ffc38aa9ed9f7e4b851aab
Dmitry <kerzhimanov@gmail.com>
2024-10-19 19:23:28 +0300

    Initial commit

---
+++ tests_12_2.py
@@ -0,0 +1,48 @@
+class Runner:
+    def __init__(self, name, speed=5):
+        self.name = name
+        self.distance = 0
+        self.speed = speed
+
+    def run(self):
+        self.distance += self.speed * 2
+        return self.distance
+
+    def walk(self):
+        self.distance += self.speed
+        return self.distance
+
+    def __str__(self):
+        return self.name
+
+    def __eq__(self, other):
+        if isinstance(other, str):
+            return self.name == other
+        elif isinstance(other, Runner):
+            return self.name == other.name
+
+
+class Tournament:
+    def __init__(self, distance, *participants):
+        self.full_distance = distance
+        self.participants = list(participants)
+
+    def start(self):
+        finishers = {}
+        place = 1
+        while self.participants:
+            for participant in self.participants:
+                participant.run()
+                if participant.distance >= self.full_distance:
+                    finishers[place] = participant
+                    place += 1
+                    self.participants.remove(participant)
+        return finishers
+runner1 = Runner("Usain", speed=10)
+runner2 = Runner("Andry", speed=9)
+runner3 = Runner("Nik", speed=3)
+
+
+race = Tournament(90, runner1, runner2, runner3)
+a = race.start()
+print(a)
