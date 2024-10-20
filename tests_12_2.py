36781801bd2f390a5ce89b867224e3ac0d0c67b0
Dmitry <kerzhimanov@gmail.com>
2024-10-20 20:19:20 +0300

    main code deletion

--- tests_12_2.py
+++ tests_12_2.py
@@ -1,87 +1,40 @@
 import unittest
 from module12_2 import *
 
-# class Runner:
-#     def __init__(self, name, speed=5):
-#         self.name = name
-#         self.distance = 0
-#         self.speed = speed
-#
-#     def run(self):
-#         self.distance += self.speed * 2
-#
-#     def walk(self):
-#         self.distance += self.speed
-#
-#     def __str__(self):
-#         return self.name
-#
-#     def __eq__(self, other):
-#         if isinstance(other, str):
-#             return self.name == other
-#         elif isinstance(other, Runner):
-#             return self.name == other.name
-#
-#     def __repr__(self):
-#         return self.name
-#
-#
-# class Tournament:
-#     def __init__(self, distance, *participants):
-#         self.full_distance = distance
-#         self.participants = list(participants)
-#
-#     def start(self):
-#         finishers = {}
-#         place = 1
-#         while self.participants:
-#             for participant in self.participants:
-#                 participant.run()
-#                 if participant.distance >= self.full_distance:
-#                     finishers[place] = participant
-#                     place += 1
-#                     self.participants.remove(participant)
-#         return finishers
-
 
 class TournamentTest(unittest.TestCase):
-
-    is_frozen = True
 
     @classmethod
     def setUpClass(cls):
         cls.all_results = {}
 
-    #@unittest.skipIf(is_frozen == True, "Тесты в этом кейсе заморожены")
+    # @unittest.skipIf(is_frozen == True, "Тесты в этом кейсе заморожены")
     def setUp(self):
         self.run1 = Runner("Usain", speed=10)
         self.run2 = Runner("Andry", speed=9)
         self.run3 = Runner("Nik", speed=3)
 
-    @unittest.skipIf(is_frozen == True, "Тесты в этом кейсе заморожены")
-    def test1_start(self):
+    def test_start1(self):
         self.race = Tournament(90, self.run1, self.run3)
         res = self.race.start()
         TournamentTest.all_results.update(res)
         self.assertIs(str(TournamentTest.all_results[2]), "Nik")
 
-    def test2_start(self):
+    def test_start2(self):
         self.race = Tournament(90, self.run2, self.run3)
         res = self.race.start()
         TournamentTest.all_results.update(res)
         self.assertIs(str(TournamentTest.all_results[2]), "Nik")
 
-    def test3_start(self):
+    def test_start3(self):
         self.race = Tournament(90, self.run1, self.run2, self.run3)
         res = self.race.start()
         TournamentTest.all_results.update(res)
         self.assertIs(str(TournamentTest.all_results[3]), "Nik")
 
     def tearDown(self):
-        # dict_ = TournamentTest.all_results
         for key, value in TournamentTest.all_results.items():
             print(key, value)
-        # print(TournamentTest.all_results)
 
     @classmethod
     def tearDownClass(cls):
