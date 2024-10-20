56737e6a036bafcb16f646e5d5d9724fde32117d
Dmitry <kerzhimanov@gmail.com>
2024-10-20 22:59:13 +0300

    changes

--- module12_4.py
+++ module12_4.py
@@ -1,4 +1,4 @@
-import logging
+#import logging
 
 class Runner:
     def __init__(self, name, speed=5):
@@ -14,9 +14,11 @@ class Runner:
 
     def run(self):
         self.distance += self.speed * 2
+        return self.distance
 
     def walk(self):
         self.distance += self.speed
+        return self.distance
 
     def __str__(self):
         return self.name
@@ -50,12 +52,13 @@ class Tournament:
         return finishers
 
 first = Runner('Вося', 10)
-second = Runner('Илья', 5)
-third = Runner('Арсен', 10)
+second = Runner(1, 5)
+# third = Runner('Арсен', 10)
+#
+# t = Tournament(101, first, second)
+# print(t.start())
 
-t = Tournament(101, first, second)
-print(t.start())
-
-if __name__ == '__main__':
-    logging.basicConfig(level=logging.INFO, filemode="w", filename="runner_test.log",
-                        format="%(asctime)s | %(levelname)s | %(message)s")
+# if __name__ == '__main__':
+#     unittest.main()
+    #logging.basicConfig(level=logging.INFO, filemode="w", filename="runner_test.log",
+                        #format="%(asctime)s | %(levelname)s | %(message)s")
--- tests_12_4.py
+++ tests_12_4.py
@@ -9,22 +9,23 @@ class RunnerTest(unittest.TestCase):
     @unittest.skipIf(is_frozen == True, 'Тесты в этом кейсе заморожены')
     def test_walk(self):
         try:
-            logging.info('"test_walk" выполнен успешно')
-            walking = Runner('Peter')
+            #logging.info('"test_walk" выполнен успешно')
+            #walking = Runner('Peter')
             for i in range(9):
-                walking.walk()
-            self.assertEqual(walking.walk(), 50)
+                first.walk()
+            logging.info('"test_walk" выполнен успешно')
+            self.assertEqual(first.walk(), 50)
         except:
             logging.warning("Неверная скорость для Runner")
 
     @unittest.skipIf(is_frozen == True, 'Тесты в этом кейсе заморожены')
     def test_run(self):
         try:
-            running = Runner('Peter')
+            #running = Runner('Peter')
             for i in range(9):
-                running.run()
-            logging.info('"test_walk" выполнен успешно')
-            self.assertEqual(running.run(), 100)
+                second.run()
+            logging.info('"test_run" выполнен успешно')
+            self.assertEqual(second.run(), 100)
         except:
             logging.warning("Неверный тип данных для объекта Runner")
 
@@ -39,5 +40,5 @@ class RunnerTest(unittest.TestCase):
 
 
 if __name__ == '__main__':
-    logging.basicConfig(level=logging.INFO, filemode="w", filename="runner_test.log",
+    logging.basicConfig(level=logging.INFO, filemode="w", filename="runner_test.log", encoding="UTF-8",
                         format="%(asctime)s | %(levelname)s | %(message)s")
