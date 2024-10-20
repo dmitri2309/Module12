4d91add5d88b7f1dc6c5dd23674718af33ef9bbf
Dmitry <kerzhimanov@gmail.com>
2024-10-20 20:21:25 +0300

    decorator removal

--- tests_12_1.py
+++ tests_12_1.py
@@ -4,23 +4,18 @@ import unittest
 
 class RunnerTest(unittest.TestCase):
 
-    is_frozen = False
-
-    @unittest.skipIf(is_frozen == True, 'Тесты в этом кейсе заморожены')
     def test_walk(self):
         walking = Runner('Peter')
         for i in range(9):
             walking.walk()
         self.assertEqual(walking.walk(), 50)
 
-    @unittest.skipIf(is_frozen == True, 'Тесты в этом кейсе заморожены')
     def test_run(self):
         running = Runner('Peter')
         for i in range(9):
             running.run()
         self.assertEqual(running.run(), 100)
 
-    @unittest.skipIf(is_frozen == True, 'Тесты в этом кейсе заморожены')
     def test_challange(self):
         walking = Runner('Peter')
         running = Runner('Peter')
@@ -28,6 +23,7 @@ class RunnerTest(unittest.TestCase):
             walking.walk()
             running.run()
         self.assertNotEqual((walking.walk(), running.run()), 100)
+
 
 if __name__ == '__main__':
     unittest.main()
