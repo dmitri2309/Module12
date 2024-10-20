cc576c6d18cefa9f69237fa160442937b05daa09
Dmitry <kerzhimanov@gmail.com>
2024-10-20 16:13:20 +0300

    some changes

--- tests_12_3.py
+++ tests_12_3.py
@@ -1,51 +1,80 @@
 import unittest
-
-class Runner:
-    def __init__(self, name, speed=5):
-        self.name = name
-        self.distance = 0
-        self.speed = speed
-
-    def run(self):
-        self.distance += self.speed * 2
-
-    def walk(self):
-        self.distance += self.speed
-
-    def __str__(self):
-        return self.name
-
-    def __eq__(self, other):
-        if isinstance(other, str):
-            return self.name == other
-        elif isinstance(other, Runner):
-            return self.name == other.name
-
-    def __repr__(self):
-        return self.name
+from module12_1 import *
+from module12_2 import *
 
 
-class Tournament:
-    def __init__(self, distance, *participants):
-        self.full_distance = distance
-        self.participants = list(participants)
+# class Runner:
+#     def __init__(self, name, speed=5):
+#         self.name = name
+#         self.distance = 0
+#         self.speed = speed
+#
+#     def run(self):
+#         self.distance += self.speed * 2
+#
+#     def walk(self):
+#         self.distance += self.speed
+#
+#     def __str__(self):
+#         return self.name
+#
+#     def __eq__(self, other):
+#         if isinstance(other, str):
+#             return self.name == other
+#         elif isinstance(other, Runner):
+#             return self.name == other.name
+#
+#     def __repr__(self):
+#         return self.name
+#
+#
+# class Tournament:
+#     def __init__(self, distance, *participants):
+#         self.full_distance = distance
+#         self.participants = list(participants)
+#
+#     def start(self):
+#         finishers = {}
+#         place = 1
+#         while self.participants:
+#             for participant in self.participants:
+#                 participant.run()
+#                 if participant.distance >= self.full_distance:
+#                     finishers[place] = participant
+#                     place += 1
+#                     self.participants.remove(participant)
+#         return finishers
 
-    def start(self):
-        finishers = {}
-        place = 1
-        while self.participants:
-            for participant in self.participants:
-                participant.run()
-                if participant.distance >= self.full_distance:
-                    finishers[place] = participant
-                    place += 1
-                    self.participants.remove(participant)
-        return finishers
+
+class RunnerTest(unittest.TestCase):
+    is_frozen = False
+
+    @unittest.skipIf(is_frozen == True, 'Тесты в этом кейсе заморожены')
+    def test_walk(self):
+        walking = Runner('Peter')
+        for i in range(9):
+            walking.walk()
+        self.assertEqual(walking.walk(), 50)
+
+    @unittest.skipIf(is_frozen == True, "Тесты в этом кейсе заморожены")
+    def test_run(self):
+        running = Runner('Peter')
+        for i in range(9):
+            running.run()
+        self.assertEqual(running.run(), 100)
+
+    @unittest.skipIf(is_frozen == True, "Тесты в этом кейсе заморожены")
+    def test_challange(self):
+        walking = Runner('Peter')
+        running = Runner('Peter')
+        for i in range(9):
+            walking.walk()
+            running.run()
+        self.assertNotEqual((walking.walk(), running.run()), 100)
+
 
 class TournamentTest(unittest.TestCase):
-
-    def __repr__(self):
-        return self.name
+    is_frozen = True
 
     @classmethod
     def setUpClass(cls):
@@ -56,18 +85,21 @@ class TournamentTest(unittest.TestCase):
         self.run2 = Runner("Andry", speed=9)
         self.run3 = Runner("Nik", speed=3)
 
+    @unittest.skipIf(is_frozen == True, "Тесты в этом кейсе заморожены")
     def test1_start(self):
         self.race = Tournament(5, self.run1, self.run3)
         res = self.race.start()
         TournamentTest.all_results.update(res)
         self.assertIs(str(TournamentTest.all_results[2]), "Nik")
 
+    @unittest.skipIf(is_frozen == True, "Тесты в этом кейсе заморожены")
     def test2_start(self):
         self.race = Tournament(5, self.run2, self.run3)
         res = self.race.start()
         TournamentTest.all_results.update(res)
         self.assertIs(str(TournamentTest.all_results[2]), "Nik")
 
+    @unittest.skipIf(is_frozen == True, "Тесты в этом кейсе заморожены")
     def test3_start(self):
         self.race = Tournament(5, self.run1, self.run2, self.run3)
         res = self.race.start()
@@ -81,6 +113,6 @@ class TournamentTest(unittest.TestCase):
     def tearDownClass(cls):
         print(cls.all_results)
 
+
 if __name__ == '__main__':
     unittest.main()
-
