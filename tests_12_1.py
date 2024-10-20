89e12ce478f568c9dd58f112510463a0422834c4
Dmitry <kerzhimanov@gmail.com>
2024-10-20 20:12:10 +0300

    main code removal

--- tests_12_1.py
+++ tests_12_1.py
@@ -1,22 +1,6 @@
 from module12_1 import *
 import unittest
 
-# class Runner:
-#     def __init__(self, name):
-#         self.name = name
-#         self.distance = 0
-#
-#     def run(self):
-#         self.distance += 10
-#         return self.distance
-#
-#
-#     def walk(self):
-#         self.distance += 5
-#         return self.distance
-#
-#     def __str__(self):
-#         return self.name
 
 class RunnerTest(unittest.TestCase):
 
