f1cdfa4307214b5f1d8bf92528039b7f45df6ebf
Dmitry <kerzhimanov@gmail.com>
2024-10-28 16:42:48 +0300

    exc_info added

--- tests_12_4.py
+++ tests_12_4.py
@@ -10,35 +10,37 @@ class RunnerTest(unittest.TestCase):
     is_frozen = False
     @unittest.skipIf(is_frozen == True, 'Тесты в этом кейсе заморожены')
     def test_walk(self):
-        first = Runner('Вася', -5)
+        #first = Runner('Вася', -5)
         #first = module12_4.Runner('Вася', -5)
         try:
-            #first = Runner('Вася', -5)
+            first = Runner('Вася', -5)
             for i in range(10):
                 first.walk()
+            #logging.info('"test_walk" выполнен успешно')
+            self.assertEqual(first.distance, 50, f"{first.name} должен пробежать 50, а пробежал {first.distance}")
             logging.info('"test_walk" выполнен успешно')
-            self.assertEqual(first.distance, 50)
         # except:
         #     logging.warning("Неверная скорость для Runner")
-        except Exception as e:
-            logging.warning(f"Неверная скорость для Runner: {e}")
+        except Exception as exc:
+            logging.warning("Неверная скорость для Runner", exc_info=exc)
 
     @unittest.skipIf(is_frozen == True, 'Тесты в этом кейсе заморожены')
     def test_run(self):
-        second = Runner(1, 5)
+        #second = Runner(1, 5)
         #second = module12_4.Runner(1, 5)
         try:
-            #second = Runner(1, 5)
+            second = Runner(1, 5)
             #running = Runner('Peter')
             for i in range(10):
                 second.run()
+            #logging.info('"test_run" выполнен успешно')
+            self.assertEqual(second.distance, 100, f"{second.name} должен пробежать 50, а пробежал {second.distance}")
             logging.info('"test_run" выполнен успешно')
-            self.assertEqual(second.distance, 100)
         # except:
         #     logging.warning("Неверный тип данных для объекта Runner")
-        except Exception as e:
+        except Exception as exc:
             #logging.exception(f"Ошибка при выполнении 'test_run': {e}")
-            logging.warning(f"Неверный тип данных для объекта Runner: {e}")
+            logging.warning("Неверный тип данных для объекта Runner", exc_info=exc)
 
 
     # @unittest.skipIf(is_frozen == True, 'Тесты в этом кейсе заморожены')
